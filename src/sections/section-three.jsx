3998989098b021b93e9012afc39ecac47148326a
Mike <codingshields@gmail.com>
2025-07-29 12:54:41 -0600

    changes to section 3 and location card

--- src/sections/section-three.jsx
+++ src/sections/section-three.jsx
@@ -1,4 +1,4 @@
-const SectionFour = () => {
+const SectionThree = () => {
   return (
     <div className="w-full lg:px-[72px] xl:px-[160px] pb-[96px] px-[24px] flex-col items-start justify-start lg:justify-center inline-flex self-stretch  gap-[10px]  max-h-[1301px] lg:max-h-[726px] h-auto mx-auto">
       <div className="self-stretch  flex flex-col lg:flex-row  justify-start items-start sm:items-center	 gap-[64px]  sm:gap-[48px]  h-auto  xl:min-h-[527px] w-full sm:max-w-[960px] max-w-[452px] mx-auto ">
@@ -16,14 +16,14 @@ const SectionFour = () => {
             But what if your one day could build something eternal?
           </div>
           <div className="max-h-[252px] h-auto max-w-[452px]  w-full ">
-            <span class="text-SB_Dark_Blue text-md font-bold font-[`futura-pt`] leading-6">
+            <span class="text-SB_Dark_Blue text-[18px] font-bold font-[`futura-pt`] leading-6">
               Your “One Day” can provide:
               <br />
             </span>
             <ul className="list-disc list-outside pl-10">
-              <li class="text-SB_Dark_Blue text-md font-bold font-[`futura-pt`] leading-normal tracking-tight">
+              <li class="text-SB_Dark_Blue text-[18px] font-bold font-[`futura-pt`] leading-normal tracking-tight">
                 A roof and solid walls:{" "}
-                <span class="text-SB_Dark_Blue text-md font-normal font-[`futura-pt`] leading-normal">
+                <span class="text-SB_Dark_Blue text-[18px] font-normal font-[`futura-pt`] leading-normal">
                   {" "}
                   A gathering place constructed with bricks protecting
                   congregations from the elements and providing a safe space to
@@ -32,9 +32,9 @@ const SectionFour = () => {
                 </span>
               </li>
 
-              <li class="text-SB_Dark_Blue text-md font-bold font-[`futura-pt`] leading-normal">
+              <li class="text-SB_Dark_Blue text-[18px] font-bold font-[`futura-pt`] leading-normal">
                 A spiritual home:{" "}
-                <span class="text-SB_Dark_Blue text-md font-normal font-[`futura-pt`] leading-normal">
+                <span class="text-SB_Dark_Blue text-[18px] font-normal font-[`futura-pt`] leading-normal">
                   {" "}
                   An environment where individuals can learn, grow in their
                   faith, and thrive in Jesus.
@@ -42,9 +42,9 @@ const SectionFour = () => {
                 </span>
               </li>
 
-              <li class="text-SB_Dark_Blue text-md font-bold font-[`futura-pt`] leading-normal">
+              <li class="text-SB_Dark_Blue text-[18px] font-bold font-[`futura-pt`] leading-normal">
                 A vital hub for the community:{" "}
-                <span class="text-SB_Dark_Blue text-md font-normal font-[`futura-pt`] leading-normal">
+                <span class="text-SB_Dark_Blue text-[18px] font-normal font-[`futura-pt`] leading-normal">
                   {" "}
                   A central point for connection, mutual support, and a beacon
                   of enduring hope.
@@ -52,9 +52,9 @@ const SectionFour = () => {
                 </span>
               </li>
 
-              <li class="text-SB_Dark_Blue text-md font-bold font-[`futura-pt`] leading-normal">
+              <li class="text-SB_Dark_Blue text-[18px] font-bold font-[`futura-pt`] leading-normal">
                 A lasting legacy of faith:{" "}
-                <span class="text-SB_Dark_Blue text-md font-normal font-[`futura-pt`] leading-normal">
+                <span class="text-SB_Dark_Blue text-[18px] font-normal font-[`futura-pt`] leading-normal">
                   {" "}
                   A wellspring, developing leaders who will go on to plant many
                   more churches and multiply the impact.
@@ -69,4 +69,4 @@ const SectionFour = () => {
   );
 };
 
-export default SectionFour;
+export default SectionThree;
