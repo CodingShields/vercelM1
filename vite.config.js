e2aa59f2fd9d2609d472996c91d5e60d588c3bf9
Mike <codingshields@gmail.com>
2025-08-01 14:53:02 -0600

    test push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@mxsbc/vite-plugin-federation";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "remoteApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": {
+          import: "./src/App.jsx",
+          dontAppendStylesToHead: true,
+        },
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
   build: {
     modulePreload: false,
     target: "esnext",
